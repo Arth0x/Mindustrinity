94b21e71f9772e9c4772343db2975a9e6912cecb
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-01 10:33:08 +0100

    x

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,5 +1,1 @@
-require('Planet');
-var mod = Vars.mods.locateMod("mindustrinity");
-mod.meta.displayName = "[blue]Mind[yellow]ustri[red]nity";
-mod.meta.description = "[blue]Aether - [yellow]Iridium - [red]Vulcanite";
-mod.meta.author = "[blue]Arthox";
+require("units/copters")
