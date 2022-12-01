3e79e79e66d88959a19f57e66a90325914faa4f6
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-01 10:51:58 +0100

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,6 @@
+const terribleium = Item("terribleium");
+terribleium.color = Color.valueOf("ff0000");
+//...
 
-// listen for the event where a unit is destroyed
-Events.on(UnitDestroyEvent, event => {
-    // display toast on top of screen when the unit was a player
-    if(event.unit.isPlayer()){
-      Vars.ui.hudfrag.showToast("Pathetic.");
-    }
-  })
-  
+
   
