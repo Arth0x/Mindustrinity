5c3738b2622e2b0b5373ded76718b38990ddfb1d
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-01 11:35:57 +0100

    x

--- dev-corner/main.js
+++
@@ -1,5 +0,0 @@
-require('Planet');
-var mod = Vars.mods.locateMod("mindustrinity");
-mod.meta.displayName = "[blue]Mind[yellow]ustri[red]nity";
-mod.meta.description = "[blue]Aether - [yellow]Iridium - [red]Vulcanite";
-mod.meta.author = "[blue]Arthox";
