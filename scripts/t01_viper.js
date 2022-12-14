8d60381ace96beb5906407c5ac1b33a1d3b50791
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-14 16:26:11 +0100

    Update t01_viper.js

--- scripts/t01_viper.js
+++ scripts/t01_viper.js
@@ -4,5 +4,5 @@ const t01_viper = extend(UnitType, "Viper", {
 });
 
 t01_viper.constructor = () => extend(UnitEntity, {});
-Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(t01_viper, 60 * 25, ItemStack.with(Items.silicon, 15, Items.iron, 20)));
+Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(t01_viper, 60 * 25, ItemStack.with(Items.silicon, 15, Items.titanium, 20)));
 
