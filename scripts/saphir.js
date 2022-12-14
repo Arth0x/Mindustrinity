177a7434c34baeb7b5284a9e1ee250e2db3c4530
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-14 15:36:48 +0100

    Update saphir.js

--- scripts/saphir.js
+++ scripts/saphir.js
@@ -1,6 +1,4 @@
-const saphir = extendContent(UnitType, "saphir", {
+const SpoNavT1 = extendContent(UnitType, "saphir", {
 });
-saphir.constructor = () => extend(UnitWaterMove, {});
-saphir.ammoType = AmmoTypes.power;
-const diamondItem = Vars.content.getByName(ContentType.item, "mindustrinity-iron");
-Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(saphir, 60 * 25, ItemStack.with(Items.silicon, 15, Items.sporePod, 15, Items.thorium, 5)));
+Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(SpoNavT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.sporePod, 15, Items.thorium, 5)));
+
