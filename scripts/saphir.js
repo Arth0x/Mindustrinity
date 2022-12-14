ad7f2a34aefde1b20cb8a36e40bd78cb22b6ef73
Arthox <61596950+Arth0x@users.noreply.github.com>
2022-12-14 15:55:05 +0100

    Update saphir.js

--- scripts/saphir.js
+++ scripts/saphir.js
@@ -1,4 +1,22 @@
-const SpoNavT1 = extendContent(UnitType, "saphir", {
+
+
+const saphir = extend(UnitType, "Saphir", {
+	draw(unit) {
+		this.super$draw(unit);
+		Draw.rect(
+			"mindustrinity-t3A_copter-rotor",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
+			Time.time * -15);
+		Draw.rect(
+			"mindustrinity-t3A_copter-rotor-outline",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
+			Time.time * -15);
+	}
+    
 });
-Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(SpoNavT1, 60 * 25, ItemStack.with(Items.silicon, 15, Items.sporePod, 15, Items.thorium, 5)));
+
+saphir.constructor = () => extend(UnitEntity, {});
+Blocks.navalFactory.plans.add(new UnitFactory.UnitPlan(saphir, 60 * 25, ItemStack.with(Items.silicon, 15, Items.sporePod, 15, Items.thorium, 5)));
 
