6de5f8456e6805b143623731bbc508df8ae2e06f
Arthox <61596950+Arth0x@users.noreply.github.com>
2025-06-08 01:35:06 +0200

    q

--- scripts/t01-orvet.js
+++
@@ -1,7 +0,0 @@
-
-
-const t01-orvet = extend(UnitType, "t01-orvet", {
-});
-
-t01-orvet.constructor = () => extend(UnitEntity, {});
-Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(t01-orvet, 60 * 25, ItemStack.with(Items.silicon, 15, Items.titanium, 20)));
