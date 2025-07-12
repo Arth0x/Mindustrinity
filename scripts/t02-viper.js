7acc51b83f6167a8331672c31fc5cce426f924c2
Arthox <61596950+Arth0x@users.noreply.github.com>
2025-07-12 20:07:36 +0200

    Update t02-viper.js

--- scripts/t02-viper.js
+++ scripts/t02-viper.js
@@ -1,12 +1,12 @@
 const t02_viper = extend(UnitType, "t02-viper", {
-    // Ajoute ici les propriétés de l’unité si besoin
+    // Ajoute ici les propriétés (health, speed, etc.) si besoin
 });
 
 t02_viper.constructor = () => extend(UnitEntity, {});
 
 Events.on(ClientLoadEvent, e => {
-    Blocks.additiveReconstructor.upgrades.addAll([
-        [Vars.content.getByName(ContentType.unit, "mindustrinity-t01-orvet"),
-         Vars.content.getByName(ContentType.unit, "mindustrinity-t02-viper")]
-    ]);
+    Blocks.additiveReconstructor.upgrades.add(
+        Vars.content.getByName(ContentType.unit, "mindustrinity-t01-orvet"),
+        Vars.content.getByName(ContentType.unit, "mindustrinity-t02-viper")
+    );
 });
